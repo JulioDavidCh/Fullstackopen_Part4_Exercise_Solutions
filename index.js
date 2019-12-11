b0834c8bdb676287c659acdef12b02a19fa6ee01
Julio <juliodavidchopite@gmail.com>
2019-12-11 20:21:36 -0300

    separated the blog schema into its own module

--- index.js
+++ index.js
@@ -5,36 +5,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
-
-mongoose.set('useFindAndModify', false)
-
-const blogSchema = mongoose.Schema({
-    title: {type: String, required: true, minlength: 3},
-    author: {type: String, minlength:3},
-    url: {type: String, required: true, unique: true},
-    likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-blogSchema.plugin(uniqueValidator)
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-.then(response => {
-  console.log('connected to mongodb !')
-})
+const Blog = require('./models/blog')
 
 app.use(express.static('build'))
 app.use(bodyParser.json())
---
+++ models/blog.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const mongoUrl = process.env.MONGODB_URI
+
+console.log('connecting to :', mongoUrl)
+
+mongoose.set('useFindAndModify', false)
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+.then(response => {
+  console.log('connected to mongodb !')
+})
+
+const blogSchema = mongoose.Schema({
+  title: {type: String, required: true, minlength: 3},
+  author: {type: String, minlength:3},
+  url: {type: String, required: true, unique: true},
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+transform: (document, returnedObject) => {
+  returnedObject.id = returnedObject._id.toString()
+  delete returnedObject._id
+  delete returnedObject.__v
+}
+})
+
+blogSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Blog', blogSchema)
