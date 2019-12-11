79313e03186cfeb0f8920ff99ca71c47ef32abf1
Julio <juliodavidchopite@gmail.com>
2019-12-10 23:59:53 -0300

    the prototype project

---
+++ build/precache-manifest.c910c9aacbb7692636ac3d9810c15764.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59be47ff258ee4dfc1a56fb2531633b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a2b7055878568480ea",
+    "url": "/static/js/2.a58f5117.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.a58f5117.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6eaf89303c93bbfd3f2",
+    "url": "/static/js/main.30c0e1cc.chunk.js"
+  },
+  {
+    "revision": "754a61196a312c21b560",
+    "url": "/static/js/runtime-main.5057fe56.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,135 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+mongoose.set('useFindAndModify', false)
+
+const blogSchema = mongoose.Schema({
+    title: {type: String, required: true, minlength: 3},
+    author: {type: String, minlength:3},
+    url: {type: String, required: true, unique: true},
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+blogSchema.plugin(uniqueValidator)
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'your url here'
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+.then(response => {
+  console.log('connected to mongodb !')
+})
+
+app.use(express.static('build'))
+app.use(bodyParser.json())
+app.use(cors())
+ 
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req,res)
+  ].join(' ')
+}))
+
+// ----------------- ROUTE HANDLERS --------------------
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+    
+app.get('/api/blogs', (req, res) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        res.json(blogs.map(entry => entry.toJSON()))
+      })
+      
+  })
+  
+app.post('/api/blogs', (req, res, next) => {
+  console.log(req.body)
+  const blog = new Blog(req.body)
+  blog
+    .save()
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/blogs/:id', (req, res, next) => {
+  const id =  req.params.id
+  console.log(id)
+
+  Blog
+  .findByIdAndDelete(id)
+  .then(response => {
+    res.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/blogs/:id', (req, res, next) => {
+  const id = req.params.id
+  const body = req.body
+
+  const newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog
+  .findByIdAndUpdate(id, newBlog, { new: true })
+  .then(updatedBlog =>{
+    res.json(updatedBlog.toJSON).end
+  })
+  .catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error)
+  if(error.name === 'CastError' && error.kind === 'ObjectId'){
+    return res.status(400).send({error: 'malformatted id'})
+  }else if (error.name === 'ValidationError'){
+    return res.status(400).send({ error:error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
