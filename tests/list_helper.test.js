b42de28cac973e9cf20c55b3f2175d12c4e9974b
Julio <juliodavidchopite@gmail.com>
2019-12-12 02:39:33 -0300

    added unit tests for most liked blogs

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -51,22 +51,23 @@ const blogs = [
   }  
 ]
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
 test('Dummy test', () => {
   const result = listhelper.dummy(blogs)
   expect(result).toBe(1)
 })
 
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
+describe('Total likes', () => {
 
   test('when list is empty, likes equals zero', () => {
     const result = listhelper.totalLikes([])
@@ -81,5 +82,31 @@ describe('total likes', () => {
   test('when list has multiple blogs, is calculated right', () => {
     const result = listhelper.totalLikes(blogs)
     expect(result).toBe(36)
+  })
+})
+
+describe('Most liked', () => {
+
+  test('when list is empty, most liked equals empty object {}', () => {
+    const result = listhelper.mostLikes([{}])
+    expect(result).toEqual({})
+  })
+
+  test('when list only has one blog, equals itself', () => {
+    const result = listhelper.mostLikes(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  test('when list has multiple blogs, returns the one with the most likes', () => {
+    const result = listhelper.mostLikes(blogs)
+    expect(result).toEqual(
+      {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    })
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const mostLikes = (blogs) => {
+  const reducer = (mostLiked, nextBlog) => {
+    if(nextBlog.likes > mostLiked.likes){
+      return nextBlog
+    }else{
+      return mostLiked
+    }
+  }
+  return blogs.reduce(reducer)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  mostLikes
 }
