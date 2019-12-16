e79bb18d6797125949bae421637a1bffb4e06d21
Julio <juliodavidchopite@gmail.com>
2019-12-16 07:17:22 -0300

    made the delete handler for blogs use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -27,35 +27,48 @@ blogRouter.post('/', (req, res, next) => {
     .catch(error => next(error))
 })
 
-blogRouter.delete('/:id', (req, res, next) => {
-const id =  req.params.id
-console.log(id)
+blogRouter.delete('/:id', async (req, res, next) => {
+  try{
+    const id =  req.params.id
+  
+    await Blog.findByIdAndDelete(id)
+    
+    res.status(204).end()
 
-Blog
-.findByIdAndDelete(id)
-.then(response => {
-  res.status(204).end()
+  }catch(error){
+    next(error)
+  }
 })
-.catch(error => next(error))
+
+blogRouter.delete('/:id', async (req, res, next) => {
+  try{
+    const id =  req.params.id
+    console.log(id)
+  
+    await Blog.findByIdAndDelete(id)
+    res.status(204).end()
+  }catch(error){
+    next(error)
+  }
 })
 
 blogRouter.put('/:id', (req, res, next) => {
-const id = req.params.id
-const body = req.body
+  const id = req.params.id
+  const body = req.body
 
-const newBlog = {
-  title: body.title,
-  author: body.author,
-  url: body.url,
-  likes: body.likes
-}
+  const newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
 
-Blog
-.findByIdAndUpdate(id, newBlog, { new: true })
-.then(updatedBlog =>{
-  res.json(updatedBlog.toJSON).end
-})
-.catch(error => next(error))
+  Blog
+  .findByIdAndUpdate(id, newBlog, { new: true })
+  .then(updatedBlog =>{
+    res.json(updatedBlog.toJSON).end
+  })
+  .catch(error => next(error))
 })
 
 module.exports = blogRouter
