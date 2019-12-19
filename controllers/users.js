90d86cf59b898fc19c0f7599b029ae3104b83400
Julio <juliodavidchopite@gmail.com>
2019-12-19 20:44:23 -0300

    added conditions to generate error for the password field of requests

--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,24 @@ const bcrypt = require('bcryptjs')
 usersRouter.post('/', async (req, res, next)=> {
   try{
     const body = req.body
-
     const saltRound = 10
+
+    if(!body.password){
+      let error = Error('Validation error: password field missing')
+      error.name = 'ValidationError'
+      throw error
+    }
+    if(body.password.length < 3){
+      let error = Error('Validation error: Your password is too short, it must 3 or more characters long')
+      error.name = 'ValidationError'
+      throw error
+    }
+
     const passwordHash = await bcrypt.hash(body.password, saltRound)
 
     const newUser = new User({
       username: body.username,
-      name: body.username,
+      name: body.name,
       passwordHash
     })
 
