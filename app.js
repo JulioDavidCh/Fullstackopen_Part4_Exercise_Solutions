b7702b29396359f24cbf5166e58da8e547a7823f
Julio <juliodavidchopite@gmail.com>
2019-12-20 16:33:47 -0300

    added our login router to our app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 console.log('connecting to', config.MONGODB_URI)
 
@@ -23,6 +24,7 @@ app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(middleware.morganMiddleware)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', userRouter)
 app.use('/api/blogs', blogRouter)
 
