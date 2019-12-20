b4c4a6bb83b6a295babc33e970e5d3c89fe0ac76
Julio <juliodavidchopite@gmail.com>
2019-12-20 11:36:55 -0300

    expanded both blogs and users so that they are populated when the client sends a get request for both controllers

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,30 +1,45 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // blogRouter.get('/', (req, res) => {
 //   res.send('<h1>Hello World!</h1>')
 // })
   
 blogRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', {username: 1, name: 1})
+
   res.json(blogs.map(entry => entry.toJSON()))
 })
 
-blogRouter.post('/', (req, res, next) => {
-  const newBlogToAdd = {
-    title: req.body.title,
-    author: req.body.author,
-    url: req.body.url,
-    likes: req.body.likes || 0
-  }
+blogRouter.post('/', async (req, res, next) => {
+  try{
+    const body = req.body
 
-  const blog = new Blog(newBlogToAdd)
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-    .catch(error => next(error))
+    const userInDb = await User.findById(body.userId)
+  
+    const newBlogToAdd = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: userInDb._id
+    }
+
+    const blogToAdd = new Blog(newBlogToAdd)
+    const addedBlog = await blogToAdd.save(newBlogToAdd)
+
+    userInDb.blogs = userInDb.blogs.concat(addedBlog.id)
+
+    await userInDb.save()
+
+    res.status(201).json(addedBlog)
+
+  }catch(exception){
+    next(exception)
+  }
 })
 
 blogRouter.delete('/:id', async (req, res, next) => {
